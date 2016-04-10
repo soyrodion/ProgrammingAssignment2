b372e9b11a3978a30d05a7f84044173357f0d873
Leonardo Jimenez R <soyrodion@gmail.com>
2016-04-10 16:46:28 -0700

    Commiting cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -4,12 +4,29 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    inv<-NULL
+    set<-function(y){
+        x<<-y
+        inv<<-NULL
+    }
+    get<-function() x
+    setInv<-function(i) inv <<-i
+    getInv<-function() inv
+    list(set=set, get=get, 
+         setInv=setInv,getInv=getInv)
 }
-
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+    inv<-x$getInv()
+    if(!is.null(inv)){
+        message("geting cached data")
+        return(inv)
+    }
+    data<- x$get()
+    inv<-solve(data)
+    x$setInv(inv)
+    inv
 }
